d1d325f027bf06da8eaf176ebdeae2f19810e6bd
Peter Bouda <pbouda@cidles.eu>
2012-12-18 12:13:42 +0000

    AnnotationGraph.elements() first draft.

--- src/poioapi/annotationgraph.py
+++ src/poioapi/annotationgraph.py
@@ -9,6 +9,9 @@
 
 from __future__ import unicode_literals
 
+import os.path
+import codecs
+
 from poioapi import data
 from graf import GraphParser
 
@@ -27,11 +30,11 @@ class AnnotationGraph():
             self.structure_type_handler = None
 
         self.graf = None
+        self.graf_basename = None
 
 
     def load_graph_from_graf(self, filepath):
-        """Load the project annotation graph from a pickle
-        file.
+        """Load the project annotation graph from a GrAF/XML file.
 
         Parameters
         ----------
@@ -39,6 +42,31 @@ class AnnotationGraph():
             The absolute path to a file.
 
         """
-
         parser = GraphParser()
         self.graf = parser.parse(filepath)
+        (self.graf_basename, _) = os.path.splitext(os.path.abspath(filepath))
+
+    def elements(self):
+        """Retrieve the elements from the annotation graph. Elements are
+        sub-graph that have the root node of the data structure type as
+        the root node.
+
+        Returns
+        -------
+        e : array_like
+            Return elements of the graph.
+
+        """
+        # read the base data to get string for regions
+        base_data_file = self.graf_basename + ".txt"
+        f = codecs.open(base_data_file, "r", "utf-8")
+        base_data = f.read()
+        f.close()
+
+        base_tier_name =  self.structure_type_handler.flat_data_hierarchy[0]
+        for (node_id, node) in self.graf.nodes.items():
+            if node_id.startswith(base_tier_name):
+                region = node.links[0][0]
+                utterance = base_data[region.start:region.end]
+        return []
+
