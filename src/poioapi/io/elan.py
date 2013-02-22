a180c170f9db48c3694d33809e76e3e1f2606b1e
LopesAntonioM <lopes.antoniomr@gmail.com>
2013-02-22 14:38:18 +0000

    Elan module updated.

--- src/poioapi/io/elan.py
+++ src/poioapi/io/elan.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
+#
 # Poio Tools for Linguists
 #
 # Copyright (C) 2009-2013 Poio Project
 # Author: António Lopes <alopes@cidles.eu>
 # URL: <http://media.cidles.eu/poio/>
 # For license information, see LICENSE.TXT
+
 """This module contains classes to access Elan data.
 The class Eaf is a low level API to .eaf files.
 
