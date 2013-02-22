8b1b1002c8353ef035db2a207d1080a7931dd149
LopesAntonioM <lopes.antoniomr@gmail.com>
2013-02-22 15:25:06 +0000

    Typecraft tests passing.

--- src/poioapi/tests/test_typecraft.py
+++ src/poioapi/tests/test_typecraft.py
@@ -8,6 +8,9 @@
 # For license information, see LICENSE.TXT
 
 import os
+import re
+
+import xml.etree.ElementTree as ET
 
 from poioapi.io import typecraft
 
@@ -20,8 +23,50 @@ class TestTypecraft:
 
     def setup(self):
         self.filename = os.path.join(os.path.dirname(__file__), "sample_files",
-            "typecraft_graf", "typecraft_example.eaf")
+            "typecraft_graf", "typecraft_example.xml")
 
         self.basedirname = os.path.dirname(self.filename)
 
         self.typecraft = typecraft.Typecraft(self.filename)
+
+        self.graph = self.typecraft.typecraft_to_elan()
+
+        tree = ET.parse(self.filename)
+        self.root = tree.getroot()
+
+        self.xml_namespace = re.search('\{(.*)\}', self.root.tag).group()
+
+    def test_phrase_nodes(self):
+        nodes_number = len(self.root.findall(self.xml_namespace+"phrase"))
+
+        expected_nodes_number = 0
+
+        for nodes in self.graph.nodes:
+            if "phrase" in nodes.id:
+                expected_nodes_number+=1
+
+        assert(nodes_number == expected_nodes_number)
+
+    def test_phrase_annotation_features(self):
+        """ The annotations of a phrase are the
+        attributes and the rest elements
+        excluding the WORD and globaltags elements.
+
+        """
+
+        node_phrase = self.root.find(self.xml_namespace+"phrase")
+
+        expected_features_number = len(node_phrase.attrib)
+
+        for elements in node_phrase:
+            key = str(elements.tag).split(self.xml_namespace)
+            if key[1] != "word" and key[1] != "globaltags":
+                expected_features_number+=1
+
+        node = self.graph.nodes["phrase/n1818"]
+
+        node_annotations = node.annotations._elements
+
+        features_number = len(node_annotations[0].features)
+
+        assert(features_number == expected_features_number)
