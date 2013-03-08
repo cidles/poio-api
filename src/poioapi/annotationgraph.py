2bf866417dd9788a806b8cbbb49274c1592a0b26
LopesAntonioM <lopes.antoniomr@gmail.com>
2013-03-08 16:05:20 +0000

    Methods generate_graf_files and from_elan updated.

--- src/poioapi/annotationgraph.py
+++ src/poioapi/annotationgraph.py
@@ -276,14 +276,7 @@ class AnnotationGraph():
         # Create a GrAF object
         self.graf = elan.as_graf()
 
-        # Gather all the information to generate the GrAF files
-        self.basedirname = elan.basedirname
-        self.structure_type_handler = elan.data_structure_hierarchy
         self.data_structure_constraints = elan.data_structure_constraints
-        self.elan_file = True
-        self.filename = elan.filename
-        self.filepath = elan.filepath
-        self.xml_files_map = elan.xml_files_map
 
     def from_typecraft(self, typecraftfile):
         """This method generates a GrAF object
@@ -310,7 +303,7 @@ class AnnotationGraph():
 
         pass
 
-    def generate_graf_files(self):
+    def generate_graf_files(self, inputfile, outputfile):
         """This method will create the GrAF Xml files.
         But first is need to create the GrAF object in
         order to get the values.
@@ -325,36 +318,39 @@ class AnnotationGraph():
 
         """
 
-        self.header = poioapi.io.header.HeaderFile(self.basedirname)
-        self.header.filename = os.path.splitext(self.filename)[0]
-        self.header.primaryfile = self.filename
-        self.header.dataType = 'text'
+        filename = os.path.abspath(outputfile)
+        (basedirname, file_extension) = os.path.splitext(outputfile)
+        header = poioapi.io.header.HeaderFile(basedirname)
+        header.filename = os.path.splitext(filename)[0]
+        header.primaryfile = os.path.basename(outputfile)
+        header.dataType = 'text'
 
-        for elements in self.xml_files_map.items():
+        for elements in self.graf.additional_information.items():
             file_name = elements[0]
             extension = file_name+".xml"
-            filepath = self.basedirname+"-"+extension
+            filepath = basedirname+"-"+extension
             loc = os.path.basename(filepath)
-            self.header.add_annotation(loc, file_name)
+            header.add_annotation(loc, file_name)
             file = open(filepath,'wb')
             element_tree = elements[1]
             doc = minidom.parseString(tostring(element_tree))
             file.write(doc.toprettyxml(indent='  ', encoding='utf-8'))
             file.close()
 
-        self.header.create_header()
+        header.create_header()
 
-        if self.elan_file:
-            self._generate_metafile()
+        if file_extension is ".eaf":
+            self._generate_metafile(inputfile)
 
-    def _generate_metafile(self):
+    def _generate_metafile(self, inputfile):
         """ This method will create a metafile
         from the original file.
 
         """
 
-        # Generating the metafile
-        tree = ET.parse(self.filepath)
+        (basedirname, _) = os.path.splitext(inputfile)
+
+        tree = ET.parse(inputfile)
         root = tree.getroot()
 
         # Generate the metadata file
@@ -365,7 +361,7 @@ class AnnotationGraph():
         data_structure = SubElement(header_tag, 'data_structure')
 
         SubElement(data_structure, 'hierarchy').text =\
-        str(self.structure_type_handler)
+        str(['utterance'])
 
         tier_mapping = SubElement(header_tag,'tier_mapping')
 
@@ -377,7 +373,7 @@ class AnnotationGraph():
                 SubElement(type, 'tier').text = value
 
         file_tag = SubElement(element_tree, "file",
-                {"data_type":self.header.dataType})
+                {"data_type":'elan'})
 
         miscellaneous = SubElement(file_tag, "miscellaneous")
 
@@ -397,7 +393,7 @@ class AnnotationGraph():
                        lower_child.text is not None:
                         child_element.text = lower_child.text
 
-        filename = self.basedirname+"-extinfo.xml"
+        filename = basedirname+"-extinfo.xml"
         file = open(filename,'wb')
         doc = minidom.parseString(tostring(element_tree))
         file.write(doc.toprettyxml(indent='  ', encoding='utf-8'))
