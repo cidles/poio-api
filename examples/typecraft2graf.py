9a263969741882fa7a16994ad4633ff220f449a6
LopesAntonioM <lopes.antoniomr@gmail.com>
2013-03-08 16:23:16 +0000

    Script updated.

--- examples/typecraft2graf.py
+++ examples/typecraft2graf.py
@@ -17,20 +17,22 @@ def main(argv):
     inputfile = ''
 
     try:
-        opts, args = getopt.getopt(argv,"hi:o:",["ifile="])
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
     except getopt.GetoptError:
-        print('typecraft2graf.py -i <inputfile>')
+        print('typecraft2graf.py -i <inputfile> -o <outputfile>')
         sys.exit(2)
 
     for opt, arg in opts:
         if opt == '-h':
-            print('typecraft2graf.py -i <inputfile>')
+            print('typecraft2graf.py -i <inputfile> -o <outputfile>')
             sys.exit()
         elif opt in ('-i', '--ifile'):
             inputfile = arg
+        elif opt in ('-o', '--ofile'):
+            outputfile = arg
 
-    if inputfile == "":
-        print('typecraft2graf.py -i <inputfile>')
+    if inputfile == "" or outputfile == "":
+        print('typecraft2graf.py -i <inputfile> -o <outputfile>')
         sys.exit()
 
     # Create the data structure
@@ -43,7 +45,7 @@ def main(argv):
     annotation_graph.from_typecraft(inputfile)
 
     # Generate the GrAF files
-    annotation_graph.generate_graf_files()
+    annotation_graph.generate_graf_files(inputfile, outputfile)
 
     print('Finished')
 
