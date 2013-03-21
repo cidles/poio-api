15ff17fc7d5792234523abd002abe72caa21de76
arlopes <buildbot@BrickBandDev1.(none)>
2013-03-21 12:30:39 +0000

    Tests to elan module added.

--- src/poioapi/tests/test_elan.py
+++ src/poioapi/tests/test_elan.py
@@ -31,12 +31,63 @@ class TestElan:
         self.metafile = os.path.join(os.path.dirname(__file__), "sample_files",
             "elan_graf", "example-extinfo.xml")
 
-        data_structure = ['utterance',['clause',['word']],'translation']
+        self.elan = elan.Parser(self.filename)
 
-        # Initialize the Elan class
-        self.elan = elan.Parser(self.filename, data_structure)
+    def test_get_root_tiers(self):
+        root_tiers = self.elan.get_root_tiers()
 
-    def test_write_elan(self):
-        # compare the result file with
-        # the original
+        assert len(root_tiers) == 4
+
+    def test_get_child_tiers_for_tier(self):
+
+        # Get the root tiers
+        root_tiers = self.elan.get_root_tiers()
+
+        # Select the W-Spch tier
+        tier = root_tiers[1]
+
+        child_tier = self.elan.get_child_tiers_for_tier(tier)
+
+        assert len(child_tier) == 2
+
+    def test_get_annotations_for_tier(self):
+
+        root_tiers = self.elan.get_root_tiers()
+        child_tiers = self.elan.get_child_tiers_for_tier(root_tiers[1])
+
+        tier = child_tiers[0] # W-Words
+
+        child_tier_annotations = self.elan.get_annotations_for_tier(tier)
+
+        assert len(child_tier_annotations) == 97
+
+    def test_get_annotations_for_tier_with_parent(self):
         pass
+
+    def test_tier_has_regions(self):
+
+        root_tiers = self.elan.get_root_tiers()
+        child_tiers = self.elan.get_child_tiers_for_tier(root_tiers[1])
+
+        tier = child_tiers[0] # W-Words
+
+        has_regions = self.elan.tier_has_regions(tier)
+
+        assert has_regions == True
+
+    def test_region_for_annotation(self):
+
+        root_tiers = self.elan.get_root_tiers()
+        child_tiers = self.elan.get_child_tiers_for_tier(root_tiers[1])
+
+        tier = child_tiers[0] # W-Words
+
+        annotations = self.elan.get_annotations_for_tier(tier)
+
+        annotation = annotations[0]
+
+        regions = self.elan.region_for_annotation(annotation)
+
+        expected_regions = (780, 1340)
+
+        assert regions == expected_regions
