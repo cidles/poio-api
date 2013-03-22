3dfdd1c746ba700b83bf3e61a8677850dbfbb85a
LopesAntonioM <lopes.antoniomr@gmail.com>
2013-03-22 20:09:52 +0000

    Globaltags parsing added.

--- src/poioapi/io/typecraft.py
+++ src/poioapi/io/typecraft.py
@@ -47,6 +47,7 @@ class Parser(poioapi.io.graf.BaseParser):
         self.dependencies_map['morpheme'] = {'parent': 'word', 'last_id': 1, 'id': 1}
         self.dependencies_map['gloss'] = {'parent': 'morpheme', 'last_id': 1, 'id': 1}
         self.dependencies_map['globaltags'] = {'parent': 'phrase', 'last_id': 1, 'id': 1}
+        self.dependencies_map['globaltag'] = {'parent': 'globaltags', 'last_id': 1, 'id': 1}
 
         self.tree = ET.parse(self.filepath).getroot()
         self.xml_namespace = re.search('\{(.*)\}', self.tree.tag).group()
@@ -77,6 +78,13 @@ class Parser(poioapi.io.graf.BaseParser):
                 name = "w" + str(self._next_id("word"))
                 self._position_tier_map[tier.name].append({'name': name, 'position': i})
                 tiers.append(poioapi.io.graf.Tier(name, "word"))
+
+            for (i, _) in enumerate(self.tree.findall(self.xml_namespace + tier.linguistic_type
+                                                      + "[@id='" + tier.name + "']/"
+                                                      + self.xml_namespace + "globaltags")):
+                name = "gts" + str(self._next_id("globaltags"))
+                self._position_tier_map[tier.name].append({'name': name, 'position': i})
+                tiers.append(poioapi.io.graf.Tier(name, "globaltags"))
         else:
             if tier.linguistic_type == "word":
                 words = self._find_words(tier.name)
@@ -85,6 +93,14 @@ class Parser(poioapi.io.graf.BaseParser):
                     name = "m" + str(self._next_id("morpheme"))
                     self._position_tier_map[tier.name].append({'name': name, 'position': i})
                     tiers.append(poioapi.io.graf.Tier(name, "morpheme"))
+
+            elif tier.linguistic_type == "globaltags":
+                globaltags = self._find_globaltags(tier.name)
+
+                for (i, _) in enumerate(globaltags.findall(self.xml_namespace + "globaltag")):
+                    name = "gt" + str(self._next_id("globaltag"))
+                    self._position_tier_map[tier.name].append({'name': name, 'position': i})
+                    tiers.append(poioapi.io.graf.Tier(name, "globaltag"))
 
             elif tier.linguistic_type == "morpheme":
                 (word_name, morpheme_position) = self._find_tier_position(tier.name)
@@ -134,6 +150,11 @@ class Parser(poioapi.io.graf.BaseParser):
                                          + parent_name + "']/" + self.xml_namespace
             + "word")[tier_position]
 
+        if tier.linguistic_type == "globaltags":
+            elements = self.tree.findall(self.xml_namespace + "phrase[@id='"
+                                         + parent_name + "']/" + self.xml_namespace
+            + "word")[tier_position]
+
         elif tier.linguistic_type == "morpheme":
             words = self._find_words(parent_name)
 
@@ -156,6 +177,13 @@ class Parser(poioapi.io.graf.BaseParser):
         words = self.tree.findall(self.xml_namespace + "phrase[@id='" + phrase_name + "']/" +
                                   self.xml_namespace + "word")[word_position]
         return words
+
+    def _find_globaltags(self, globaltag_name):
+        (phrase_name, globaltag_position) = self._find_tier_position(globaltag_name)
+
+        globaltags = self.tree.findall(self.xml_namespace + "phrase[@id='" + phrase_name + "']/" +
+                                  self.xml_namespace + "globaltags")[globaltag_position]
+        return globaltags
 
     def _next_id(self, name):
         current_id = self.dependencies_map[name]['id']
