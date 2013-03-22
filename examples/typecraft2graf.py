be8aba88fdb5f2eb121e4509e8ead539b1844517
LopesAntonioM <lopes.antoniomr@gmail.com>
2013-03-22 19:37:56 +0000

    Script updated.

--- examples/typecraft2graf.py
+++ examples/typecraft2graf.py
@@ -11,10 +11,6 @@ import sys, getopt
 
 import poioapi.annotationgraph
 import poioapi.data
-import poioapi.io.graf
-import poioapi.io.typecraft
-import poioapi.io.graf_old
-import poioapi.io.typecraft_old
 
 def main(argv):
 
