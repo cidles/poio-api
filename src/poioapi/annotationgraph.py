b3f5ae2ef3f4b500faa14ac174ed3cead88d5542
LopesAntonioM <lopes.antoniomr@gmail.com>
2013-04-09 15:31:57 +0100

    from_elan updated.

--- src/poioapi/annotationgraph.py
+++ src/poioapi/annotationgraph.py
@@ -284,7 +284,7 @@ class AnnotationGraph():
 
         """
 
-        if sys.version < (2,7):
+        if sys.version_info < (2,7):
             parser = poioapi.io.elan.Parser26(elanfile)
         else:
             parser = poioapi.io.elan.Parser(elanfile)
