915a6d2a791a68ce44f4d7eaf3589b14a161028f
LopesAntonioM <lopes.antoniomr@gmail.com>
2013-05-01 14:23:44 +0100

    Tests updated.

--- src/poioapi/tests/test_excel.py
+++ src/poioapi/tests/test_excel.py
@@ -22,12 +22,12 @@ class TestExcel:
 
     def setup(self):
         self.filename = os.path.join(os.path.dirname(__file__), "sample_files",
-            "excel_graf", "hinuq2.csv")
+            "excel_graf", "Hinuq2.csv")
 
         self.excel = poioapi.io.excel.Parser(self.filename)
 
         headerfile = os.path.join(os.path.dirname(__file__), "sample_files",
-            "excel_graf", "hinuq2.hdr")
+            "excel_graf", "Hinuq2.hdr")
 
         self.annotation_graph = poioapi.annotationgraph.AnnotationGraph(None)
         self.annotation_graph.load_graph_from_graf(headerfile)
@@ -44,19 +44,19 @@ class TestExcel:
         child_tiers = self.excel.get_child_tiers_for_tier(root_tier)
 
         assert len(child_tiers) == 1
-        assert child_tiers[0].name == "clause_type"
+        assert child_tiers[0].name == "clause_id"
 
-        tier = poioapi.io.graf.Tier('grammatical_relation')
+        tier = poioapi.io.graf.Tier('word')
         child_tiers = self.excel.get_child_tiers_for_tier(tier)
 
-        assert len(child_tiers) == 3
-        assert child_tiers[0].name == "part_of_spech"
+        assert len(child_tiers) == 4
+        assert child_tiers[0].name == "grammatical_relation"
 
     def test_get_annotations_for_tier(self):
         tier = poioapi.io.graf.Tier('ref')
         tier_annotations = self.excel.get_annotations_for_tier(tier)
 
-        assert len(tier_annotations) == 195
+        assert len(tier_annotations) == 10
 
     def test_get_annotations_for_tier_with_parent(self):
         root_tiers = self.excel.get_root_tiers()
@@ -68,21 +68,20 @@ class TestExcel:
 
         assert len(child_tier_annotations) == 3
         assert child_tier_annotations[0].id == "#1"
-        assert child_tier_annotations[0].value == "m"
         assert child_tier_annotations[2].id == "#3"
-        assert child_tier_annotations[2].value == "sub"
 
     def test_get_annotations_from_list(self):
         annotation_parent = poioapi.io.graf.Annotation('1', "XoddonBarun.001")
-        annotations = self.excel._get_annotations_from_list(3, annotation_parent.id)
+        annotations = self.excel._get_annotations_from_list(2, annotation_parent.id)
 
         assert len(annotations) == 3
         assert annotations[0].id == "#1"
-        assert annotations[0].value == "m"
+        assert annotations[1].id == "#2"
+        assert annotations[2].id == "#3"
 
     def test_find_parent(self):
         parent = self.excel._find_parent_id(0, 0, 1)
-        assert parent == '29'
+        assert parent == '32'
 
     def test_compare_graf_objects(self):
         graf_from_header = self.annotation_graph.graf
