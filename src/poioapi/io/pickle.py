523d6cf23d68999f44450477dab5e57214b2d1dc
arlopes <lopes.antoniomr@gmail.com>
2013-07-16 13:56:13 +0100

    Methods removed.

--- src/poioapi/io/pickle.py
+++ src/poioapi/io/pickle.py
@@ -113,7 +113,7 @@ class Parser(poioapi.io.graf.BaseParser):
                     else:
                         self._build_indices(e, t, parent)
             else:
-                # workarounf for ids that were written wrong to pickle file
+                # workaround for ids that were written wrong to pickle file
                 if elements[i]['id'] == '[':
                     elements[i]['id'] = self.last_used_id
                     self.last_used_id += 1
@@ -128,83 +128,3 @@ class Parser(poioapi.io.graf.BaseParser):
                 if t == self.data_hierarchy[0]:
                     local_parent = new_parent
 
-    # def _get_elements_for_tier(self, elements, parent_element = None, level = 0):
-    #     for position, element in enumerate(elements):
-    #         if isinstance(element, list):
-    #             if not isinstance(element[0], list):
-    #                 level += 1
-
-    #             self._get_elements_for_tier(element, parent_element, level)
-
-    #             if position + 1 <= len(elements) - 1:
-    #                 if isinstance(elements[position + 1], list):
-    #                     level -= 1
-    #         else:
-    #             key = self._find_key_in_map(level, position)
-    #             element['parent'] = parent_element
-    #             self._tier_map[key]['values'].append(element)
-
-    #             if position is 0:
-    #                 parent_element = "a{0}".format(element['id'])
-
-    # def _find_key_in_map(self, level, position):
-    #     for key, values in self._tier_map.items():
-    #         if values['position'] == position \
-    #         and values['level'] == level:
-    #             return key
-
-    #     return None
-
-    # def _find_structure_levels(self, data_hierarchy, level = 0):
-    #     for position, element in enumerate(data_hierarchy):
-    #         if isinstance(element, list):
-    #             self._find_structure_levels(element, (level + 1))
-    #         else:
-    #             self._tier_map[element] = {'level':level, 'position':position,
-    #                                        'values':[]}
-
-    # def _find_tier_in_structure(self, tier, structure):
-    #     for i, element in enumerate(structure):
-    #         if isinstance(element, list):
-    #             aux = self._find_tier_in_structure(tier, element)
-    #             if aux is not None:
-    #                 return aux
-    #             else:
-    #                 return []
-    #         else:
-    #             if element == tier.name and i == 0:
-    #                 return self._find_childs_from_structure(structure)
-
-    # def _find_childs_from_structure(self, structure):
-    #     auxliar_strucutre = []
-
-    #     for element in structure:
-    #         if element == structure[0]:
-    #             continue
-    #         if isinstance(element, list):
-    #             auxliar_strucutre.append(element[0])
-    #         else:
-    #             auxliar_strucutre.append(element)
-
-    #     return auxliar_strucutre
-
-    # def _create_raw_file(self):
-    #     """Creates an txt file with the data in the
-    #     Annotation Tree file. Passing only the sentences.
-
-    #     """
-
-    #     file = os.path.abspath(self.basedirname + '.txt')
-    #     f = codecs.open(file,'w', 'utf-8') # Need the encode
-
-    #     # Verify the elements
-    #     for element in self.annotation_tree.elements():
-
-    #         # Get the utterance
-    #         utterance = element[0]
-
-    #         # Write the content to the txt file
-    #         f.write(utterance.get('annotation') + '\n')
-
-    #     # Close txt file
-    #     f.close()
