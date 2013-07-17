16f70126a54d1cd823af8a8b8f63df4e33c7ea01
LopesAntonioM <lopes.antoniomr@gmail.com>
2013-07-17 12:21:36 +0100

    Test updated.

--- src/poioapi/tests/io/test_tcf.py
+++ src/poioapi/tests/io/test_tcf.py
@@ -35,7 +35,7 @@ class TestTcf:
     def test_get_child_tiers_for_tier(self):
         root_tiers = self.parser.get_root_tiers()
         root_child_tier = self.parser.get_child_tiers_for_tier(root_tiers[0])
-        assert root_child_tier[0].name == "token"
+        assert root_child_tier[0].name == "tokens"
 
         children_tier = self.parser.get_child_tiers_for_tier(root_child_tier[0])
         assert len(children_tier) == 2
@@ -43,14 +43,19 @@ class TestTcf:
     def test_get_annotations_for_tier(self):
         root_tiers = self.parser.get_root_tiers()
         root_annotations = self.parser.get_annotations_for_tier(root_tiers[0])
-        assert len(root_annotations) == 1
+        assert len(root_annotations) == 2
 
+        parent_annotation = poioapi.io.graf.Annotation("s1", "t1 t2 t3 t4 t5")
         token_tier = self.parser.get_child_tiers_for_tier(root_tiers[0])
-        token_annotations = self.parser.get_annotations_for_tier(token_tier[0])
-        assert len(token_annotations) == 9
+        token_annotations = self.parser.get_annotations_for_tier(token_tier[0], parent_annotation)
+        assert len(token_annotations) == 5
 
     def test_tier_has_regions(self):
-        pass
+        root_tiers = self.parser.get_root_tiers()
+        assert self.parser.tier_has_regions(root_tiers[0])
 
     def test_region_for_annotation(self):
-        pass
+        root_tiers = self.parser.get_root_tiers()
+        root_annotations = self.parser.get_annotations_for_tier(root_tiers[0])
+        region = self.parser.region_for_annotation(root_annotations[0])
+        assert region == ('1', '20')
