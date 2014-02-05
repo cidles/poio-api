76dc9788910f97b1463ba3abf5ed15d84fd00285
alopes <alopes@cidles.eu>
2014-02-05 11:51:14 +0000

    Typecraft writer updated.

--- src/poioapi/io/typecraft.py
+++ src/poioapi/io/typecraft.py
@@ -22,7 +22,6 @@ import xml.etree.cElementTree as ET
 from xml.etree.ElementTree import tostring
 from xml.dom import minidom
 
-import poioapi.io.common
 import poioapi.io.graf
 
 
@@ -180,8 +179,15 @@ class Writer(poioapi.io.graf.BaseWriter):
 
     """
 
-    def write(self, outputfile, converter, pretty_print=False, ids=None, tags=None):
+    def __init__(self):
+        self._missing_gloss_list = []
+        self._missing_pos_list = []
+        self._current_text_id = 0
+        self._current_phrase_id = 0
+        self.extra_gloss_map = None
+        self.extra_pos_map = None
 
+    def write(self, outputfile, converter, pretty_print=False, ids=None, tags=None):
         nodes = converter.graf.nodes
         phrases = self._get_phrases(nodes, converter)
         time_start_nodes = self._get_tag_nodes(nodes, "ELANBegin")
@@ -193,133 +199,127 @@ class Writer(poioapi.io.graf.BaseWriter):
         gloss_nodes = self._get_tag_nodes(nodes, "g")
         trans_nodes = self._get_tag_nodes(nodes, "f")
 
-        common_pos = poioapi.io.common.POS()
-        common_gloss = poioapi.io.common.Gloss()
-
         if ids:
             self._current_text_id = ids[0]
             self._current_phrase_id = ids[1]
+
+        if tags and tags != "null":
+            tags = tags.replace(":", "\":\"").replace("/", "\",\"").replace("{", "{\"").replace("}", "\"}")
+
+            if tags.split("_")[0] != "{EMPTY}":
+                self.extra_gloss_map = ast.literal_eval(tags.split("_")[0])
+            if tags.split("_")[1] != "{EMPTY}":
+                self.extra_pos_map = ast.literal_eval(tags.split("_")[1])
+
+        attribs = {"xsi:schemaLocation": "http://typecraft.org/typecraft.xsd",
+                   "xmlns": "http://typecraft.org/typecraft",
+                   "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance"}
+
+        root = ET.Element("typecraft", attribs)
+
+        # The language must be set as und
+        text = ET.SubElement(root, "text", {"id": self._next_text_id(), "lang": "und"})
+
+        if converter.meta_information:
+            ET.SubElement(text, "title").text = converter.meta_information
         else:
-            self._current_text_id = 0
-            self._current_phrase_id = 0
+            ET.SubElement(text, "title").text = "Empty Title"
 
-        if tags is None:
-            common_pos = self._check_for_missing_pos(pos_nodes)
-            common_gloss = self._check_for_missing_gloss(gloss_nodes)
-            wrt = ""
+        ET.SubElement(text, "titleTranslation")
+        ET.SubElement(text, "body").text = self._get_body(phrases)
 
-            if common_gloss.gloss_miss_list:
-                wrt += str(common_gloss.gloss_miss_list) + "--"
-            if common_pos.pos_miss_list:
-                if wrt == "":
-                    wrt += "--" + str(common_pos.pos_miss_list)
-                else:
-                    wrt += str(common_pos.pos_miss_list)
+        for p in phrases:
+            original = p.annotations._elements[0].features["annotation_value"]
 
-            if wrt != "":
-                file = codecs.open(outputfile + ".missing", 'wb', encoding='utf-8')
-                file.write(wrt)
-                file.close()
+            if original == "":
+                continue
+
+            phrase = ET.SubElement(text, "phrase", {"id": self._next_phrase_id(), "valid": "VALID"})
+
+            offset_node = self._get_time_related_nodes(time_start_nodes, p)
+            duration_node = self._get_time_related_nodes(time_end_nodes, p)
+            participant_node = self._get_time_related_nodes(participant_nodes, p)
+
+            # Get the time
+            if offset_node:
+                offset_value = self._string_to_milliseconds(offset_node)
+                phrase.set("offset", str(offset_value))
+
+                if duration_node:
+                    duration_value = self._string_to_milliseconds(duration_node) - offset_value
+                    phrase.set("duration", str(duration_value))
+
+            if participant_node:
+                phrase.set("speaker", participant_node.annotations._elements[0].features["annotation_value"])
+
+            ET.SubElement(phrase, "original").text = original
+
+            t = self._get_nodes_by_parent(trans_nodes, p.id)
+            if t:
+                ET.SubElement(phrase, "translation").text = t[0].annotations._elements[0].features["annotation_value"]
+            else:
+                ET.SubElement(phrase, "translation")
+
+            ET.SubElement(phrase, "description")
+            ET.SubElement(phrase, "globaltags", {"id": "1", "tagset": "Default"})
+
+            for w in self._get_nodes_by_parent(word_nodes, p.id):
+                w_value = w.annotations._elements[0].features["annotation_value"]
+                word = ET.SubElement(phrase, "word", {"text": w_value, "head": "false"})
+
+                ET.SubElement(word, "pos").text = self._validate_pos(self._get_pos_value(pos_nodes, w.id),
+                                                                     self.extra_pos_map)
+
+                for m in self._get_nodes_by_parent(morph_nodes, w.id):
+                    m_value = m.annotations._elements[0].features["annotation_value"].replace("-", "")
+
+                    morpheme = ET.SubElement(word, "morpheme", {"text": m_value, "baseform": m_value})
+
+                    for g in self._get_nodes_by_parent(gloss_nodes, m.id):
+                        if g.annotations._elements[0].features:
+                            g_value = g.annotations._elements[0].features["annotation_value"]
+                            aux = g_value.split("'- |, |= |:'")
+                            gloss_list = None
+
+                            if len(aux) is 1:
+                                gloss_list = self._validate_gloss(g_value.replace("-", ""), self.extra_gloss_map)
+
+                            if gloss_list:
+                                for gloss in gloss_list:
+                                    ET.SubElement(morpheme, "gloss").text = gloss
+                            else:
+                                morpheme.set("meaning", self._set_meaning(aux[0]))
+
+        if self._missing_gloss_list or self._missing_pos_list:
+            self.write_missing_tags(outputfile)
         else:
-            if tags != "ignore":
-                tags = tags.replace(":", "\":\"")
-                tags = tags.replace(",", "\":\"")
-                tags = tags.replace("{", "{\"")
-                tags = tags.replace("}", "\"}")
-                extra_gloss_map = ast.literal_eval(tags.split("_")[0])
-                extra_pos_map = ast.literal_eval(tags.split("_")[1])
-            else:
-                extra_gloss_map = None
-                extra_pos_map = None
-
-            attribs = {"xsi:schemaLocation": "http://typecraft.org/typecraft.xsd",
-                       "xmlns": "http://typecraft.org/typecraft",
-                       "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance"}
-
-            root = ET.Element("typecraft", attribs)
-
-            # The language must be set as und
-            text = ET.SubElement(root, "text", {"id": self._next_text_id(), "lang": "und"})
-
-            if converter.meta_information:
-                ET.SubElement(text, "title").text = converter.meta_information
-            else:
-                ET.SubElement(text, "title").text = "Empty Title"
-
-            ET.SubElement(text, "titleTranslation")
-            ET.SubElement(text, "body").text = self._get_body(phrases)
-
-            for p in phrases:
-                original = p.annotations._elements[0].features["annotation_value"]
-
-                if original == "":
-                    continue
-
-                phrase = ET.SubElement(text, "phrase", {"id": self._next_phrase_id(), "valid": "VALID"})
-
-                offset_node = self._get_time_related_nodes(time_start_nodes, p)
-                duration_node = self._get_time_related_nodes(time_end_nodes, p)
-                participant_node = self._get_time_related_nodes(participant_nodes, p)
-
-                # Get the time
-                if offset_node:
-                    offset_value = self._string_to_milliseconds(offset_node)
-                    phrase.set("offset", str(offset_value))
-
-                    if duration_node:
-                        duration_value = self._string_to_milliseconds(duration_node) - offset_value
-                        phrase.set("duration", str(duration_value))
-
-                if participant_node:
-                    phrase.set("speaker", participant_node.annotations._elements[0].features["annotation_value"])
-
-                ET.SubElement(phrase, "original").text = original
-
-                t = self._get_nodes_by_parent(trans_nodes, p.id)
-                if t:
-                    ET.SubElement(phrase, "translation").text = t[0].annotations._elements[0].features["annotation_value"]
-                else:
-                    ET.SubElement(phrase, "translation")
-
-                ET.SubElement(phrase, "description")
-                ET.SubElement(phrase, "globaltags", {"id": "1", "tagset": "Default"})
-
-                for w in self._get_nodes_by_parent(word_nodes, p.id):
-                    w_value = w.annotations._elements[0].features["annotation_value"]
-                    word = ET.SubElement(phrase, "word", {"text": w_value, "head": "false"})
-
-                    ET.SubElement(word, "pos").text = common_pos._validate_pos(self._get_pos_value(pos_nodes, w.id),
-                                                                               extra_pos_map)
-
-                    for m in self._get_nodes_by_parent(morph_nodes, w.id):
-                        m_value = m.annotations._elements[0].features["annotation_value"].replace("-", "")
-
-                        morpheme = ET.SubElement(word, "morpheme", {"text": m_value, "baseform": m_value})
-
-                        for g in self._get_nodes_by_parent(gloss_nodes, m.id):
-                            if g.annotations._elements[0].features:
-                                #g_value = g.annotations._elements[0].features["annotation_value"].replace("-", "")
-                                g_value = g.annotations._elements[0].features["annotation_value"]
-                                gloss_list = common_gloss._validate_gloss(g_value, extra_gloss_map)
-
-                                if gloss_list:
-                                    for gloss in gloss_list:
-                                        ET.SubElement(morpheme, "gloss").text = gloss
-                                else:
-                                    morpheme.set("meaning", self._set_meaning(g_value))
-
             self.write_xml(root, outputfile)
 
+    def write_missing_tags(self, outputfile):
+        wrt = ""
+
+        if self._missing_gloss_list:
+            wrt += str(self._missing_gloss_list) + "--"
+        if self._missing_pos_list:
+            if wrt == "":
+                wrt += "--" + str(self._missing_pos_list)
+            else:
+                wrt += str(self._missing_pos_list)
+
+        if wrt != "":
+            file = codecs.open(outputfile + ".missing", 'wb', encoding='utf-8')
+            file.write(wrt)
+            file.close()
 
     def write_xml(self, root, outputfile, pretty_print=True):
         if pretty_print:
             doc = minidom.parseString(tostring(root))
 
             text_re = re.compile(r'>\n\s+([^<>\s].*?)\n\s+</', re.DOTALL)
-            prettyXml = text_re.sub(r'>\g<1></', doc.toprettyxml(indent='  '))
+            pretty_xml = text_re.sub(r'>\g<1></', doc.toprettyxml(indent='  '))
 
             file = codecs.open(outputfile, 'wb', encoding='utf-8')
-            file.write(prettyXml)
+            file.write(pretty_xml)
             file.close()
         else:
             tree = ET.ElementTree(root)
@@ -422,18 +422,103 @@ class Writer(poioapi.io.graf.BaseWriter):
                 flat_elements.append(e)
         return flat_elements
 
-    def _check_for_missing_pos(self, pos_nodes):
-        common_pos = poioapi.io.common.POS()
+    def _validate_pos(self, pos_value, extra_pos_map=None):
+        """
+        This function validates the pos value
+        in a pos list from the TC pos list.
+        """
 
-        for p in pos_nodes:
-            common_pos._validate_pos(p.annotations._elements[0].features["annotation_value"])
+        pos_list = ["PNposs", "N", "V", "PN", "ADVm", "Np", "PREP", "CN", "PRT", "COMP", "AUX", "CONJS", "QUANT",
+                    "NMASC", "NNO", "NNEUT", "COP", "ART", "MOD", "PPOST", "ADJ", "DEM", "ADJC", "PNrefl", "DET", "Wh",
+                    "Vtr", "Vitr", "PROposs", "P", "Vdtr", "CARD", "NDV", "CL", "ADVplc", "V3", "PRTposs", "V4",
+                    "ADVtemp", "V2", "NUM", "REL", "EXPL", "CONJC", "Vmod", "Vlght", "PNana", "PROint", "PNrel",
+                    "VtrOBL", "COPident", "PRTpred", "Nspat", "ORD", "PTCP", "CONJ", "PRTint", "Vneg", "PREPtemp",
+                    "PREPdir", "NFEM", "PRtinf", "Ncomm", "Nbare", "Vcon", "PRTv", "PRTn", "ADVneg", "Vpre", "NUMpart",
+                    "ADJS", "PRTexist", "CLFnum", "CLFnom", "CIRCP", "V1", "PREP/PROspt", "PRTprst", "Vvector", "PNdem",
+                    "Nrel", "IPHON", "ADV", "VitrOBL", "Vimprs", "Vrefl", "PNabs", "Vbid", "Vvec", "INTRJCT"]
 
-        return common_pos
+        pos_map = {"prn": "PN", "interj": "INTRJCT", "CVB": "V", "pron": "PN", "part": "PRT", "intj": "INTRJCT",
+                   "name": "Np", "encl": "CL", "pred": "COP"}
 
-    def _check_for_missing_gloss(self, gloss_nodes):
-        common_gloss = poioapi.io.common.Gloss()
+        value = None
 
-        for g in gloss_nodes:
-            common_gloss._validate_gloss(g.annotations._elements[0].features["annotation_value"])
+        if "-" not in pos_value:
+            for pos in pos_list:
+                if pos_value.upper() == pos.upper():
+                    value = pos
+                    continue
 
-        return common_gloss
+            if pos_value in pos_map.keys():
+                value = pos_map[pos_value]
+            elif extra_pos_map and pos_value in extra_pos_map.keys():
+                value = extra_pos_map[pos_value]
+
+            if value is None:
+                if pos_value not in self._missing_pos_list:
+                    self._missing_pos_list.append(pos_value)
+
+        return value
+
+    def _validate_gloss(self, gloss_value, extra_gloss_map=None):
+        """
+            This function validates the gloss value
+            in a gloss list from the TC gloss list.
+            """
+        gloss_list = ["AGR", "ST", "STR", "W", "CONS", "ANIM", "HUM", "INANIM", "ADD",
+                      "ASP", "CESSIVE", "CMPL", "CON", "CONT", "CUST", "DUR", "DYN", "EGR",
+                      "FREQ", "HAB", "INCEP", "INCH", "INCOMPL", "INGR", "INTS", "IPFV", "ITER",
+                      "PFV", "PNCT", "PROG", "PRSTV", "REP", "RESLT", "SEMF", "STAT", "NCOMPL",
+                      "PRF", "FV", "IV", "ABES", "ABL", "ABS", "ACC", "ADES", "ALL", "BEN",
+                      "case marker - underspecified", "COMIT", "CONTL", "DAT", "DEL", "DEST",
+                      "ELAT", "EQT", "ERG", "ESS", "GEN", "ILL", "INESS", "INSTR", "LAT", "MALF",
+                      "NOM", "OBL", "PERL", "POSS", "PRTV", "TER", "VIAL", "VOC", "ORN", "CLITcomp",
+                      "CLITadv", "CLITdet", "CLITn", "CLITp", "CLITpron", "CLITv", "ATV", "FAM", "INFOC",
+                      "RFTL", "TPID", "UNID", "APPROX", "DIST", "DIST2", "DXS", "MEDIAL", "PROX", "AD",
+                      "ADJ>ADV", "ADJ>N", "ADJ>V", "AUG", "DIM", "INCORP", "KIN", "N>ADJ", "N>ADV", "NMLZ",
+                      "N>N", "NUM>N", "N>V", "PTCP>ADJ", "QUAL", "V>ADJ", "V>ADV", "vbl", ">Vitr", "V>N",
+                      ">Vtr", "V>Nagt", "V>Ninstr", "ACAUS", "APASS", "APPL", "CAUS", "PASS", "ASRT",
+                      "DECL", "EXCL", "IMP", "IMP1", "IMP2", "IND", "INTR", "MAVM", "PROH", "Q", "RPS",
+                      "COMM", "FEM", "MASC", "NEUT", "COMPL", "DO", "ICV", "OBJ", "OBJ2", "objcogn", "OBJind",
+                      "OM", "SBJ", "SC", "SM", "AFFMT", "CNTV", "COND", "CONJ", "CONTP", "EVID", "IRR", "JUSS",
+                      "MOD", "OPT", "RLS", "SBJV", "OBLIG", "POT", "MNR", "MO", "CL", "CL1", "CL10", "CL11",
+                      "CL12", "CL13", "CL14", "CL15", "CL16", "CL17", "CL18", "CL2", "CL20", "CL21", "CL22",
+                      "CL23", "CL3", "CL4", "CL5", "CL6", "CL7", "CL8", "CL9", "Npref", "landmark", "DU",
+                      "PL", "SG", "x", "DISTRIB", "1excl", "1incl", "1PL", "1SG", "2PL", "2SG",
+                      "3B", "3PL", "3Pobv", "3Pprox", "3SG", "3Y", "4", "AREAL", "PLassc", "FT", "H", "!H",
+                      "L", "MT", "RT", "NEGPOL", "POSPOL", "DEF", "INDEF", "SGbare", "SPECF", "PART", "2HML", "2HON",
+                      "HON", "TTL", "AGT", "EXP", "GOAL", "PSSEE", "PSSOR", "PT", "SRC", "TH", "CIRCM", "CTed", "DIR",
+                      "ENDPNT", "EXT", "INT", "LINE", "LOC", "PATH", "PRL", "SPTL", "STARTPNT", "VIAPNT", "DM", "MU",
+                      "PS", "ANT", "AOR", "AUX", "FUT", "FUTclose", "FUTim", "FUTnear", "FUTrel", "FUTrm", "NF", "NFUT",
+                      "NPAST", "PAST", "PASThst", "PASTim", "PASTpast", "PASTrel", "PASTrm", "PRES", "PRET", "FOC",
+                      "TOP", "CNJ", "CLF", "CV", "GER", "GERDV", "INF", "itr", "PRED", "PTCP", "SUPN", "Vstem", "ACTV",
+                      "?", "ABB", "ABSTR", "ADJstem", "ADV>ADJ", "ASS", "ASSOC", "ATT", "CMPR", "CO-EV", "CONSEC",
+                      "CONTR", "COP", "DEG", "DEM", "DIR-SP", "EMPH", "EXPLET", "GNR", "IND-SP", "K1", "K2", "LOCREL",
+                      "NEG", "Nstem", "oBEN", "PPOSTstem", "PRIV", "QUOT", "RECP", "REDP", "REFL", "REL", "RP-SP",
+                      "sBEN", "SLCT", "SUP"]
+
+        gloss_map = {"1": "CL1", "2": "CL2", "3": "CL3", "5": "CL5", "6": "CL6", "7": "CL7",
+                     "8": "CL8", "9": "CL9", "14": "CL14", "15": "CL15", "16": "CL16",
+                     "17": "CL17", "18": "CL18", "NPST": "NPAST", "EMP": "EMPH", "EXCLAM": "EXCL",
+                     "COM": "COMIT", "PERF": "PFV", "CLAS": "CLF"}
+
+        valid_glosses = []
+
+        # split gloss values
+        for gl in gloss_value.split("."):
+            for gloss in gloss_list:
+                if gl.upper() == gloss.upper():
+                    valid_glosses.append(gloss)
+
+            if gl in gloss_map.keys():
+                valid_glosses.append(gloss_map[gl])
+            elif extra_gloss_map and gl in extra_gloss_map:
+                valid_glosses.append(extra_gloss_map[gl])
+
+        if valid_glosses:
+            return valid_glosses
+        else:
+            if gloss_value.isupper():
+                if gloss_value not in self._missing_gloss_list:
+                        self._missing_gloss_list.append(gloss_value)
+
+            return None
